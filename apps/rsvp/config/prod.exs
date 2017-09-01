0895c537358f82e836a89a824c5e33d8bc2fe4e6
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-09-01 02:26:45 -0400

    Prod.exs Fixed

--- apps/rsvp/config/prod.exs
+++ apps/rsvp/config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config: :rsvp, Rsvp.Repo,
+config :rsvp, Rsvp.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
