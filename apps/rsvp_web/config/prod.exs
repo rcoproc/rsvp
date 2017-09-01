436d9675491cd9a55d5a54f3390f1ffae5129af5
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-09-01 02:29:33 -0400

    Prod.exs Fixed

--- apps/rsvp_web/config/prod.exs
+++ apps/rsvp_web/config/prod.exs
@@ -16,7 +16,7 @@ config :rsvp_web, RsvpWeb.Endpoint,
   url: [scheme: "https", host: "https://rocky-reef-71441.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
